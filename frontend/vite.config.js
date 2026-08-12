e150c3729a565cb47c3301e230783fe73c0ce816
abajuantokan-web <aba.juantokan@gmail.com>
2026-08-13 00:26:04 +0700

    gais CORS dulu

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,21 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: true,
+    port: 5154,
+    strictPort: true,
+    hmr: {
+      host: "127.0.0.1",
+    },
+    proxy: {
+      "/api": {
+        target: "https://penelitian-ilmiah-1-production.up.railway.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
