88dcbb8ed83100c5b95f294f3fab3894243bb4d6
abajuantokan-web <Lew0tana>
2026-09-01 02:24:13 +0700

    cors lagi deh

--- openpeo-admin/vite.config.js
+++ openpeo-admin/vite.config.js
@@ -7,6 +7,18 @@ export default defineConfig({
     vue(),
   ],
   server: {
-    port: 5175
-  }
+    host: true,
+    port: 5154,
+    strictPort: true,
+    hmr: {
+      host: "127.0.0.1",
+    },
+    proxy: {
+      "/api": {
+        target: "https://penelitian-ilmiah-1-production.up.railway.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
